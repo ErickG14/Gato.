3c6fc1ff815974d9c3c2aae2fe872be68dea1028
ErickG14 <124231061+ErickG14@users.noreply.github.com>
2023-02-22 19:15:52 -0600

    Add files via upload
    Garcia Sebastian Luis Erick  ICO

---
+++ Gato.java
@@ -0,0 +1,19 @@
+public class Gato{
+
+        public static void main(String[] args){
+
+System.out.println("_____________________");
+System.out.println("|______|______|_____|");
+System.out.println("|______|______|_____|");
+System.out.println("|______|______|_____|");
+
+
+
+
+
+}
+
+
+}
+
+
